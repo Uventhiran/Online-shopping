f083e6d0ae73f424b51db45294fc67af69452ee2
Uventhiran <36724172+Uventhiran@users.noreply.github.com>
2020-07-01 16:27:25 +0530

    API and Business

---
+++ BackEnd/OnlineShopping/OnlineShopping.Business/LoginBusiness.cs
@@ -0,0 +1,22 @@
+﻿namespace OnlineShopping.Business
+{
+    using OnlineShopping.Common.Interfaces;
+    using OnlineShopping.Common.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LoginBusiness : ILoginBusiness
+    {      
+        public LoginBusiness()
+        {           
+        }
+
+        public Task<string> LoginUser(UserModel userModel)
+        {
+           var Result = "success";
+           return Task.FromResult(Result);
+        }
+    }
+}
---
+++ BackEnd/OnlineShopping/OnlineShopping/Startup.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OnlineShopping.Business;
+using OnlineShopping.Common.Interfaces;
+
+namespace OnlineShopping
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped(typeof(ILoginBusiness), typeof(LoginBusiness));
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
