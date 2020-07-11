73c7530a5aa9f2f667dc48e1bb1854938a47c642
Uventhiran <36724172+Uventhiran@users.noreply.github.com>
2020-07-11 16:20:17 +0530

    DB migration

---
+++ BackEnd/OnlineShopping/OnlineShopping.Data/OnlineShoppingContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
+using OnlineShopping.Common.Entities;
+
+namespace OnlineShopping.Data
+{
+    public class OnlineShoppingContext : DbContext
+    {
+        public DbSet<User> user { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=OnlineShopping;Trusted_Connection=True;");
+        }
+    }
+}
